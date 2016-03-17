e14daa4b0bc79e45b0b8ac1c13119455a7a3c44d
Diamon <Zhilyaev.dmitriy@gmail.com>
2016-03-17 03:49:24 +0300

    Подсветка строк

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,35 @@
 function AddRow(id,col1,col2) {
     var table = document.getElementById(id);
     var row = table.insertRow(-1);
+
+    var lsg = JSON.parse(localStorage["GoodList"]);
+    var lsb = JSON.parse(localStorage["BadList"]);
+
+    for (var i=0;i<lsg.length;i++){
+        if(col1==lsg[i]){
+            row.classList.add("success");
+            break;
+        }
+    }
+
+    for (var i=0;i<lsb.length;i++){
+        if(col1==lsb[i]){
+            row.classList.add("danger");
+            break;
+        }
+    }
+
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = col1;
-    cell2.innerHTML = col2;
+    cell2.innerHTML = "<b class='badge'>"+col2+"</b>";
 }
-//# AddRow("Anal","www.vk.com","8000");
-$("#UpTime").html("Aптайм = "+localStorage["UpTime"]+" сек");
+
+$("#UpTime").html(localStorage["UpTime"]);
 
 for (var key in localStorage){
-    if(key!=="UpTime"){
+    if(key!=="UpTime" && key!=="GoodList" && key!=="BadList"){
+        //# AddRow("Anal","www.vk.com","8000");
         AddRow("Anal",key,localStorage[key]);
     }
 }
